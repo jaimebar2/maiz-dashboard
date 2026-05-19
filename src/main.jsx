1d7d534831318603d8d47dd7555f57cbbe402fd4
Jaime Barrios <jaimeyapia2601@gmail.com>
2026-05-19 14:23:25 -0500

    cambios

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom'
 
 import './index.css'
 
-import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
-
-import ProtectedRoute from './components/ProtectedRoute'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-
-        {/* LOGIN */}
-        <Route path='/' element={<Login />} />
-
-        {/* DASHBOARD PROTEGIDO */}
-        <Route
-          path='/dashboard'
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* REDIRECCIÓN */}
-        <Route
-          path='*'
-          element={<Navigate to='/' />}
-        />
-
-      </Routes>
-    </BrowserRouter>
+    <Dashboard />
   </React.StrictMode>
 )
