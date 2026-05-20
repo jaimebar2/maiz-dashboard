1a204f40c5a87a471d52d960b9342a58992c5fe2
Jaime Barrios <jaimeyapia2601@gmail.com>
2026-05-19 20:33:00 -0500

    Actualizacion del dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 
-import Dashboard from './pages/Dashboard'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Dashboard />
+    <App />
   </React.StrictMode>
 )
