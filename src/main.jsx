b11a42d11bb35c38dcb0d6e2fff0b8474d75084b
Jaime Barrios <jaimeyapia2601@gmail.com>
2026-05-19 20:48:24 -0500

    Sistema login y logout

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom'
+
 import './index.css'
 
 import App from './App'
+import Login from './pages/Login'
+
+import ProtectedRoute from './components/ProtectedRoute'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+
+      <Routes>
+
+        {/* LOGIN */}
+        <Route path='/' element={<Login />} />
+
+        {/* APP PROTEGIDA */}
+        <Route
+          path='/app'
+          element={
+            <ProtectedRoute>
+              <App />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* REDIRECCIÓN */}
+        <Route
+          path='*'
+          element={<Navigate to='/' />}
+        />
+
+      </Routes>
+
+    </BrowserRouter>
   </React.StrictMode>
 )
